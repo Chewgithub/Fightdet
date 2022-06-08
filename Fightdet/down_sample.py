a87ae8a8dcf466f622bb418fa9e04fc1551925da
Eng Yong <jack950717@hotmail.com>
2022-06-08 14:13:49 +0800

    video to numpy changes, down_sample.py, get stats.py update

---
+++ Fightdet/down_sample.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+def down_sampling(video_array, target_frames=64):
+    '''
+Reads a video array that is more than 64 frames and down sample it to 64 frames
+without changing the dimensions (frame, height, width, channel)
+Parameters:
+    video_array: numpy array of len(video_array) more 64
+
+Returns:
+    ndarray of (64, height, width, channel)
+'''
+    # get total frames of input video and calculate sampling interval
+    len_frames = int(len(video_array))
+    interval = int(np.ceil(len_frames/target_frames))
+    # init empty list for sampled video and
+    sampled_video = []
+    for i in range(0,len_frames,interval):
+        sampled_video.append(video_array[i])
+    # calculate numer of padded frames and fix it
+    num_pad = target_frames - len(sampled_video)
+    padding = []
+    if num_pad>0:
+        for i in range(-num_pad,0):
+            try:
+                padding.append(video_array[i])
+            except:
+                padding.append(video_array[0])
+        sampled_video += padding
+    # get sampled video
+    return np.array(sampled_video, dtype=np.float32)
--- Fightdet/video_to_numpy.py
+++ Fightdet/video_to_numpy.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2 as cv
 from Fightdet.ysget_optical_flow import get_optical_flow as gof
 
-def video_to_numpy(video_file_path, resize=(224,224), grayscale=False, get_optical_flow=False):
+def video_to_numpy(video_file_path, resize=(224,224), grayscale=False, optical_only=False):
     '''
     Reads a video at video_filepath and converts it to a 4-D ndarray with dimensions (frame, height, width, channel).
     If get_optical_flow is True, returns the ndarray with the last dimension containing the optical flows.
@@ -14,39 +14,124 @@ def video_to_numpy(video_file_path, resize=(224,224), grayscale=False, get_optic
     Returns:
         ndarray of (frame, height, width, channel)
     '''
+
     cap = cv.VideoCapture(video_file_path)
 
     # initialize the video as an empty list
     video_array = []
+    gray_video_array=[]
+    if optical_only:
+        while cap.isOpened():
+            # if no frame is read, close the video and exit
+            ret, frame = cap.read()
+            if not ret:
+                cap.release()
+                break
+            # resize the video if a resize shape is given
+            if resize is not None:
+                frame = cv.resize(frame, dsize=resize, interpolation=cv.INTER_AREA)
+                frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
 
-    # read and append additional frames to the list; resize and color if needed
-    while cap.isOpened():
-        ret, frame = cap.read()
-        # if no frame is read, close the video and exit
-        if not ret:
-            cap.release()
-            break
+                # join the next frame to the video
+                video_array.append(frame)
+        video_array = np.array(video_array)
+        optical=gof(video_array)
+        return optical
 
-        # resize the video if a resize shape is given
-        if resize is not None:
-            frame = cv.resize(frame, dsize=resize, interpolation=cv.INTER_AREA)
-        # convert OpenCV BGR format to Grayscale or RGB
-        if grayscale:
-            frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-            frame = np.expand_dims(frame, -1)
-        else:
-            frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-        # join the next frame to the video
-        video_array.append(frame)
+    elif grayscale:
+        while cap.isOpened():
+            # if no frame is read, close the video and exit
+            ret, frame = cap.read()
+            if not ret:
+                cap.release()
+                break
+            # resize the video if a resize shape is given
+            if resize is not None:
+                frame = cv.resize(frame, dsize=resize, interpolation=cv.INTER_AREA)
+                frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+                video_array.append(frame)
 
-    # convert list of frames to an array
-    video_array = np.array(video_array)
+                grayframe = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+                # join the next frame to the video
+                gray_video_array.append(grayframe)
 
-    if get_optical_flow:
-        # add 2 optical flow channels
-        return gof(video_array)
+        gray_video_array = np.array(gray_video_array)
+
+        optical=gof(video_array)
+        result = np.zeros((len(gray_video_array),224,224,3))
+        result[...,:1] = gray_video_array
+        result[...,1:] = optical
+        return result
+
+
     else:
-        return video_array
+        while cap.isOpened():
+            # if no frame is read, close the video and exit
+            ret, frame = cap.read()
+            if not ret:
+                cap.release()
+                break
+
+            # resize the video if a resize shape is given
+            if resize is not None:
+                frame = cv.resize(frame, dsize=resize, interpolation=cv.INTER_AREA)
+                frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+                video_array.append(frame)
+
+        video_array = np.array(video_array)
+        optical=gof(video_array)
+        result = np.zeros((len(video_array),224,224,3))
+        result[...,:3] = video_array
+        result[...,3:] = optical
+        return result
+
+
+    # # read and append additional frames to the list; resize and color if needed
+    # while cap.isOpened():
+    #     ret, frame = cap.read()
+    #     # if no frame is read, close the video and exit
+    #     if not ret:
+    #         cap.release()
+    #         break
+
+    #     # resize the video if a resize shape is given
+    #     if resize is not None:
+    #         frame = cv.resize(frame, dsize=resize, interpolation=cv.INTER_AREA)
+    #     # convert OpenCV BGR format to Grayscale or RGB
+
+    #     if optical_only:
+    #         frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+    #         video_array.append(frame)
+
+    #     if grayscale:
+    #         frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    #         frame = np.expand_dims(frame, -1)
+    #     else:
+    #         frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+    #     # join the next frame to the video
+    #     video_array.append(frame)
+
+    # # convert list of frames to an array
+    # video_array = np.array(video_array)
+
+    # if get_optical_flow and grayscale:
+    #     # add 2 optical flow channels
+    #     optical=gof(video_array)
+    #     result = np.zeros((len(video_array),224,224,3))
+    #     result[...,:1] = video_array
+    #     result[...,1:] = optical
+    #     return result
+
+    # elif get_optical_flow:
+    #     # add 2 optical flow channels
+    #     optical=gof(video_array)
+    #     result = np.zeros((len(video_array),224,224,5))
+    #     result[...,:3] = video_array
+    #     result[...,3:] = optical
+    #     return result
+
+    # else:
+    #     return video_array
 
 
 if __name__=="__main__":
